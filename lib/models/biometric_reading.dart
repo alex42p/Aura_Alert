eced4c7e8c9b2dab749f9946707d86cbf6a8d323
alex42p <64346793+alex42p@users.noreply.github.com>
2025-10-14 03:56:45 -0400

    updated biometric id to non-nullable

--- lib/models/biometric_reading.dart
+++ lib/models/biometric_reading.dart
@@ -1,10 +1,10 @@
 class BiometricReading {
-  final int? id;
+  final int id;
   final DateTime timestamp;
   final double value;
   final String type; // 'hr', 'temp', 'o2'
 
-  BiometricReading({this.id, required this.timestamp, required this.value, required this.type});
+  BiometricReading({required this.id, required this.timestamp, required this.value, required this.type});
 
   Map<String, Object?> toMap() => {
         'id': id,
@@ -14,7 +14,7 @@ class BiometricReading {
       };
 
   factory BiometricReading.fromMap(Map<String, Object?> m) => BiometricReading(
-        id: m['id'] as int?,
+        id: m['id'] as int,
         timestamp: DateTime.parse(m['timestamp'] as String),
         value: (m['value'] as num).toDouble(),
         type: m['type'] as String,
