7e6ffa09c45f216ddf7b389238d204b54def8cb1
alex42p <64346793+alex42p@users.noreply.github.com>
2025-12-01 11:43:23 -0500

    wip

---
+++ lib/services/algorithm_service.dart
@@ -0,0 +1,12 @@
+
+
+/// Algorithm Service: processes data and runs stress calculation algorithm.
+/// Weights applied to each of the biometric readings to compute a stress score
+/// for the average of the most recent 5 minutes of data.
+class AlgorithmService {
+  void runAlgorithm() {
+    // Implementation of the algorithm
+    print('Algorithm is running...');
+  }
+  
+}
---
+++ lib/services/notification_service.dart
@@ -0,0 +1,9 @@
+
+
+
+class NotificationService {
+  void sendNotification(String message) {
+    // Implementation for sending a notification
+    print('Notification sent: $message');
+  }
+}
